d9afe027106913e2aaa4b3c3a67349bb6a1bb89a
brachy84 <brachy84@gmail.com>
2022-07-08 21:05:11 +0200

    map bracket handlers to strings

--- src/main/java/com/cleanroommc/groovyscript/api/BracketHandler.java
+++ src/main/java/com/cleanroommc/groovyscript/api/BracketHandler.java
@@ -3,40 +3,42 @@ package com.cleanroommc.groovyscript.api;
 import com.cleanroommc.groovyscript.helper.recipe.ItemStack;
 import com.cleanroommc.groovyscript.helper.recipe.OreDictIngredient;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 
 public class BracketHandler {
 
     public static final List<Function<String, Object>> handlers = new ArrayList<>();
+    private static final Map<String, Function<String, Object>> bracketHandlers = new HashMap<>();
+    public static final Function<String, Object> itemStackBracketHandler = s -> {
+        try {
+            return ItemStack.parse(s);
+        } catch (Exception e) {
+            return null;
+        }
+    };
 
-    public static void registerBracketHandler(Function<String, Object> handler) {
-        handlers.add(handler);
+    public static void registerBracketHandler(String key, Function<String, Object> handler) {
+        if (key == null || key.isEmpty() || handler == null) throw new NullPointerException();
+        if (bracketHandlers.containsKey(key)) {
+            throw new IllegalArgumentException("Bracket handler already exists for key " + key);
+        }
+        bracketHandlers.put(key, handler);
     }
 
     public static Object handleBracket(String s) {
-        for (Function<String, Object> handler : handlers) {
-            Object result = handler.apply(s);
-            if (result != null) return result;
+        String[] parts = s.split(":", 2);
+        if (parts.length == 0) return s;
+        Function<String, Object> bracketHandler = bracketHandlers.get(parts[0]);
+        Object result = bracketHandler == null ? itemStackBracketHandler.apply(s) : bracketHandler.apply(parts[1]);
+        if (result == null) {
+            throw new NoSuchElementException("Could not find game object for <" + s + ">!");
         }
-        return s;
+        return result;
     }
 
     public static void init() {
-        registerBracketHandler(s -> {
-            String[] parts = s.split(":");
-            if (parts.length == 2 && parts[0].equals("ore")) {
-                return new OreDictIngredient(parts[1]);
-            }
-            return null;
-        });
-        registerBracketHandler(s -> {
-            try {
-                return ItemStack.parse(s);
-            } catch (Exception e) {
-                return null;
-            }
-        });
+        registerBracketHandler("ore", OreDictIngredient::new);
+        registerBracketHandler("item", itemStackBracketHandler);
     }
 }
