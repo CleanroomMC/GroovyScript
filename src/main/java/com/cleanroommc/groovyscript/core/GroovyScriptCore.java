8943c58864820fcb03478414f954977c6120dc90
brachy84 <brachy84@gmail.com>
2022-09-04 16:02:52 +0200

    make groovy map return fast util map

--- src/main/java/com/cleanroommc/groovyscript/core/GroovyScriptCore.java
+++ src/main/java/com/cleanroommc/groovyscript/core/GroovyScriptCore.java
@@ -31,12 +31,11 @@ public class GroovyScriptCore implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public void injectData(Map<String, Object> data) {
-
     }
 
     @Override
     public String getAccessTransformerClass() {
-        return null;
+        return "com.cleanroommc.groovyscript.core.GroovyScriptTransformer";
     }
 
     @Override
