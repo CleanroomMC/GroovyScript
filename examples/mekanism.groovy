892aaafbf8c9c7bb601a191f51cc818716f80e98
brachy84 <brachy84@gmail.com>
2022-09-05 13:40:56 +0200

    add examples

---
+++ examples/mekanism.groovy
@@ -0,0 +1,8 @@
+
+mods.mekanism.Crusher.add('<item:minecraft:clay_ball>', '<minecraft:gold_ingot>')
+
+print Arrays.toString('<ore:blockWool>'.getMatchingStacks())
+
+mods.mekanism.Crusher.removeByInput('<ore:ingotSilver>')
+
+// test chemical infuser
