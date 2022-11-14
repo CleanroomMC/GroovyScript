fb55d951c8e79515e9a04481872e0ead8a25cebc
brachy84 <brachy84@gmail.com>
2022-11-14 17:03:23 +0100

    0.0.2

--- src/main/java/com/cleanroommc/groovyscript/GroovyScript.java
+++ src/main/java/com/cleanroommc/groovyscript/GroovyScript.java
@@ -35,7 +35,7 @@ public class GroovyScript {
 
     public static final String ID = "groovyscript";
     public static final String NAME = "GroovyScript";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     public static final Logger LOGGER = LogManager.getLogger(ID);
 
