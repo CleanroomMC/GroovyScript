b12c7d816098b13008343e16bdd0e2a0868f46c6
Dylan Wong <dylanwong@nevada.unr.edu>
2022-11-26 19:30:59 -0800

    Added Infusion and Arcane Crafting recipe registries

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/Thaumcraft.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/Thaumcraft.java
@@ -5,8 +5,12 @@ import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
 public class Thaumcraft extends ModPropertyContainer {
 
     public final Crucible crucible = new Crucible();
+    public final InfusionCrafting infusionCrafting = new InfusionCrafting();
+    public final ArcaneWorkbench arcaneWorkbench = new ArcaneWorkbench();
 
     public Thaumcraft() {
         addRegistry(crucible);
+        addRegistry(infusionCrafting);
+        addRegistry(arcaneWorkbench);
     }
 }
