c05db0a54725ed58a1e3ba759e247022413d0530
Dylan Wong <dylanwong@nevada.unr.edu>
2022-11-26 19:26:00 -0800

    Added Warp interface for Thaumcraft compat

---
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/Warp.java
@@ -0,0 +1,20 @@
+package com.cleanroommc.groovyscript.compat.mods.thaumcraft;
+
+import net.minecraft.item.ItemStack;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.internal.CommonInternals;
+
+import java.util.Arrays;
+
+public class Warp {
+
+    public void addWarp(ItemStack item, int amount) {
+        ThaumcraftApi.addWarpToItem(item, amount);
+    }
+
+    public void removeWarp(ItemStack item) {
+        if (CommonInternals.warpMap.containsKey(Arrays.asList(item.getItem(), item.getItemDamage())))
+            CommonInternals.warpMap.remove(Arrays.asList(item.getItem(), item.getItemDamage()));
+    }
+
+}
