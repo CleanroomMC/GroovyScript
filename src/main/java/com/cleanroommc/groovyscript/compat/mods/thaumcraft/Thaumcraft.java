62cbb47ec61a91adaa3a38ac5222d4b3407c49be
Dylan Wong <dylanwong@nevada.unr.edu>
2022-11-30 21:50:59 -0800

    created methods for returning instances of compat classes

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/Thaumcraft.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/Thaumcraft.java
@@ -7,6 +7,13 @@ public class Thaumcraft extends ModPropertyContainer {
     public final Crucible crucible = new Crucible();
     public final InfusionCrafting infusionCrafting = new InfusionCrafting();
     public final ArcaneWorkbench arcaneWorkbench = new ArcaneWorkbench();
+    public Aspect Aspect = new Aspect();
+    public LootBag LootBag = new LootBag();
+    public Warp Warp = new Warp();
+    public SmeltingBonus SmeltingBonus = new SmeltingBonus();
+    public Research Research = new Research();
+    public DustTrigger DustTrigger = new DustTrigger();
+    public AspectHelper AspectHelper() { return new AspectHelper(); }
 
     public Thaumcraft() {
         addRegistry(crucible);
