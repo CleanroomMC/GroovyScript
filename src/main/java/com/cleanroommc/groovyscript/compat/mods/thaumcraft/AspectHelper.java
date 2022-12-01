97b62ff81b2554854e6a2b61606a4f552b32f606
Dylan Wong <dylanwong@nevada.unr.edu>
2022-11-30 21:48:37 -0800

    created AspectHelper.java to handle adding aspects to items

---
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/AspectHelper.java
@@ -0,0 +1,68 @@
+package com.cleanroommc.groovyscript.compat.mods.thaumcraft;
+
+import com.cleanroommc.groovyscript.api.GroovyLog;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.AspectEventProxy;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.aspects.AspectRegistryEvent;
+
+public class AspectHelper {
+
+    private String entity;
+    private ItemStack object;
+    private AspectList aspects;
+
+    public AspectHelper() {
+        //do nothing
+    }
+
+    public AspectHelper entity(String entity) {
+        this.entity = entity;
+        return this;
+    }
+
+    public AspectHelper object(ItemStack object) {
+        this.object = object;
+        return this;
+    }
+
+    public AspectHelper aspects(AspectList aspects) {
+        this.aspects = aspects;
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public AspectHelper stripAspects() {
+        if (entity != null) {
+            ThaumcraftApi.registerEntityTag(entity, new AspectList(), new ThaumcraftApi.EntityTagsNBT[0]);
+        } else if (object != null) {
+            ThaumcraftApi.registerObjectTag(object, new AspectList());
+        } else {
+            GroovyLog.msg("Error removing Thaumcraft Aspects from item/entity")
+                    .error()
+                    .post();
+        }
+        AspectRegistryEvent are = new AspectRegistryEvent();
+        are.register = new AspectEventProxy();
+        MinecraftForge.EVENT_BUS.post(are);
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public void register() {
+        if (entity != null) {
+            ThaumcraftApi.registerEntityTag(entity, aspects, new ThaumcraftApi.EntityTagsNBT[0]);
+        } else if (object != null) {
+            ThaumcraftApi.registerObjectTag(object, aspects);
+        } else {
+            GroovyLog.msg("Error adding Thaumcraft Aspects to item/entity")
+                    .error()
+                    .post();
+        }
+        AspectRegistryEvent are = new AspectRegistryEvent();
+        are.register = new AspectEventProxy();
+        MinecraftForge.EVENT_BUS.post(are);
+    }
+}
