f211d70bab9b28be2853802487c24b2367805374
Dylan Wong <dylanwong@nevada.unr.edu>
2022-12-04 11:14:32 -0800

    added getCommon getUncommon getRare

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/LootBag.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/LootBag.java
@@ -9,59 +9,72 @@ import java.util.*;
 
 public class LootBag {
 
+    private int type;
+
     public LootBag() {
-        //do nothing
     }
 
-    public void addItem(ItemStack item, int chance, int[] bags) {
-        ThaumcraftApi.addLootBagItem(item, chance, bags);
+    public LootBag(int type) {
+        this.type = type;
     }
 
-    public void removeItem(ItemStack item, int[] bags) {
-        for (int i : bags){
-            ArrayList<WeightedRandomLoot> list = new ArrayList<WeightedRandomLoot>();
-            switch (i) {
-                case 0:
-                    list = WeightedRandomLoot.lootBagCommon;
-                    break;
-                case 1:
-                    list = WeightedRandomLoot.lootBagUncommon;
-                    break;
-                case 2:
-                    list = WeightedRandomLoot.lootBagRare;
-                    break;
-                default:
-                    GroovyLog.msg("Error: Thaumcraft Lootbag type out of bounds, [0-2] applicable.").error().post();
-            }
-            List<WeightedRandomLoot> remove = new ArrayList<>();
-            for (WeightedRandomLoot loot : list) {
-                if (item.isItemEqual(loot.item))
-                    remove.add(loot);
-            }
-            list.removeAll(remove);
+    public static LootBag getCommon() {
+        return new LootBag(0);
+    }
+
+    public static LootBag getUncommon() {
+        return new LootBag(1);
+    }
+
+    public static LootBag getRare() {
+        return new LootBag(2);
+    }
+
+    public void addItem(ItemStack item, int chance) {
+        ThaumcraftApi.addLootBagItem(item, chance, new int[]{type});
+    }
+
+    public void removeItem(ItemStack item) {
+        ArrayList<WeightedRandomLoot> list = new ArrayList<WeightedRandomLoot>();
+        switch (type) {
+            case 0:
+                list = WeightedRandomLoot.lootBagCommon;
+                break;
+            case 1:
+                list = WeightedRandomLoot.lootBagUncommon;
+                break;
+            case 2:
+                list = WeightedRandomLoot.lootBagRare;
+                break;
+            default:
+                GroovyLog.msg("Error: Thaumcraft Lootbag type not specified. Please use Lootbag.getCommon(), Lootbag.getUncommon(), or Lootbag.getRare().").error().post();
         }
-    }
-
-    public void removeAll(int[] bags) {
-        for (int i : bags){
-            ArrayList<WeightedRandomLoot> list = new ArrayList<WeightedRandomLoot>();
-            switch (i) {
-                case 0:
-                    list = WeightedRandomLoot.lootBagCommon;
-                    break;
-                case 1:
-                    list = WeightedRandomLoot.lootBagUncommon;
-                    break;
-                case 2:
-                    list = WeightedRandomLoot.lootBagRare;
-                    break;
-                default:
-                    GroovyLog.msg("Error: Thaumcraft Lootbag type out of bounds, [0-2] applicable.").error().post();
-            }
-            List<WeightedRandomLoot> remove = new ArrayList<>();
-            for (WeightedRandomLoot loot : list)
+        List<WeightedRandomLoot> remove = new ArrayList<>();
+        for (WeightedRandomLoot loot : list) {
+            if (item.isItemEqual(loot.item))
                 remove.add(loot);
-            list.removeAll(remove);
         }
+        list.removeAll(remove);
+    }
+
+    public void removeAll() {
+        ArrayList<WeightedRandomLoot> list = new ArrayList<WeightedRandomLoot>();
+        switch (type) {
+            case 0:
+                list = WeightedRandomLoot.lootBagCommon;
+                break;
+            case 1:
+                list = WeightedRandomLoot.lootBagUncommon;
+                break;
+            case 2:
+                list = WeightedRandomLoot.lootBagRare;
+                break;
+            default:
+                GroovyLog.msg("Error: Thaumcraft Lootbag type not specified. Please use Lootbag.getCommon(), Lootbag.getUncommon(), or Lootbag.getRare().").error().post();
+        }
+        List<WeightedRandomLoot> remove = new ArrayList<>();
+        for (WeightedRandomLoot loot : list)
+            remove.add(loot);
+        list.removeAll(remove);
     }
 }
