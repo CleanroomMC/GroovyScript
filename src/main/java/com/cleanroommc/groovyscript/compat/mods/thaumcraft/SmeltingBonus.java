4e4623bd5f63a2be1fa3c641228d0527f92f89b9
Dylan Wong <dylanwong@nevada.unr.edu>
2022-12-06 00:54:18 -0800

    made class reloadable, added oreDic support

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/SmeltingBonus.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/SmeltingBonus.java
@@ -1,31 +1,70 @@
 package com.cleanroommc.groovyscript.compat.mods.thaumcraft;
 
+import com.cleanroommc.groovyscript.api.GroovyBlacklist;
+import com.cleanroommc.groovyscript.api.GroovyLog;
+import com.cleanroommc.groovyscript.api.IIngredient;
+import com.cleanroommc.groovyscript.compat.mods.ModSupport;
+import com.cleanroommc.groovyscript.helper.ingredient.ItemsIngredient;
+import com.cleanroommc.groovyscript.helper.ingredient.OreDictIngredient;
+import com.cleanroommc.groovyscript.registry.VirtualizedRegistry;
 import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.ApiStatus;
 import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.internal.CommonInternals;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SmeltingBonus {
+public class SmeltingBonus extends VirtualizedRegistry<ArrayList<Object>> {
 
-    public SmeltingBonus() {
-        //do nothing
+    public SmeltingBonus() { super("SmeltingBonus", "smelting_bonus"); }
+
+    @Override
+    @GroovyBlacklist
+    @ApiStatus.Internal
+    public void onReload() {
+        removeScripted().forEach(recipe -> {
+            this.removeByOutput((ItemStack) recipe.get(1));
+        });
+        restoreFromBackup().forEach(recipe -> {
+            this.add((IIngredient) recipe.get(0), (ItemStack) recipe.get(1), (float) recipe.get(2));
+        });
     }
 
-    public void add(ItemStack in, ItemStack out) {
-        ThaumcraftApi.addSmeltingBonus(in, out);
+    public void add(IIngredient in, ItemStack out) {
+        this.add(in, out, 0.33F);
     }
 
-    public void add(ItemStack in, ItemStack out, float chance) {
-        ThaumcraftApi.addSmeltingBonus(in, out, chance);
+    public void add(IIngredient in, ItemStack out, float chance) {
+        if (in instanceof OreDictIngredient) {
+            ThaumcraftApi.addSmeltingBonus(((OreDictIngredient) in).getOreDict(), out, chance);
+            ArrayList<Object> sb = new ArrayList<Object>();
+            sb.add(((OreDictIngredient) in).getOreDict());
+            sb.add(out);
+            sb.add(chance);
+            addScripted(sb);
+        } else if (in.getMatchingStacks().length == 1) {
+            ThaumcraftApi.addSmeltingBonus(in.getMatchingStacks()[0], out, chance);
+            ArrayList<Object> sb = new ArrayList<Object>();
+            sb.add(in.getMatchingStacks()[0]);
+            sb.add(out);
+            sb.add(chance);
+            addScripted(sb);
+        }
     }
 
     public void removeByOutput(ItemStack output) {
         List<ThaumcraftApi.SmeltBonus> remove = new ArrayList<>();
-        for (ThaumcraftApi.SmeltBonus bonus : CommonInternals.smeltingBonus)
-            if (output.isItemEqual(bonus.out))
+        for (ThaumcraftApi.SmeltBonus bonus : CommonInternals.smeltingBonus) {
+            if (output.isItemEqual(bonus.out)) {
                 remove.add(bonus);
+                ArrayList<Object> sb = new ArrayList<Object>();
+                sb.add(bonus.in);
+                sb.add(bonus.out);
+                sb.add(bonus.chance);
+                addBackup(sb);
+            }
+        }
         CommonInternals.smeltingBonus.removeAll(remove);
     }
 
@@ -33,11 +72,11 @@ public class SmeltingBonus {
 
     public static class SmeltingBonusBuilder {
 
-        private ItemStack in;
-        private ItemStack out;
-        private float chance = 0.33F;
+        private IIngredient in = null;
+        private ItemStack out = null;
+        private float chance = 0;
 
-        public SmeltingBonusBuilder input(ItemStack input) {
+        public SmeltingBonusBuilder input(IIngredient input) {
             this.in = input;
             return this;
         }
@@ -52,8 +91,26 @@ public class SmeltingBonus {
             return this;
         }
 
+        private boolean validate() {
+            if (in == null) {
+                GroovyLog.msg("Error while adding Smelting Bonus: input() must not be null.").error().post();
+                return false;
+            } else if (out == null) {
+                GroovyLog.msg("Error while adding Smelting Bonus: output() must not be null.").error().post();
+                return false;
+            } else if (chance < 0) {
+                chance = 0;
+            }
+            return true;
+        }
+
         public void register() {
-            ThaumcraftApi.addSmeltingBonus(in, out, chance);
+            if (validate()) {
+                if (chance == 0)
+                    ModSupport.THAUMCRAFT.get().smeltingBonus.add(in, out);
+                else
+                    ModSupport.THAUMCRAFT.get().smeltingBonus.add(in, out, chance);
+            }
         }
     }
 }
