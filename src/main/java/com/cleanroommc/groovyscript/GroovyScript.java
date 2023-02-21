a965fa2521a563b716f5f744754b66ffddb52669
brachy84 <brachy84@gmail.com>
2023-02-21 09:19:40 +0100

    0.3.1

--- src/main/java/com/cleanroommc/groovyscript/GroovyScript.java
+++ src/main/java/com/cleanroommc/groovyscript/GroovyScript.java
@@ -61,7 +61,7 @@ public class GroovyScript {
 
     public static final String ID = "groovyscript";
     public static final String NAME = "GroovyScript";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
 
     public static final String MC_VERSION = "1.12.2";
     public static final String GROOVY_VERSION = "4.0.8";
