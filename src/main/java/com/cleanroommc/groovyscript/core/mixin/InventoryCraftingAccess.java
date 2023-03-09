de3ad9bfdae0eba3e74e54ce3b300b80042c9ca6
brachy84 <brachy84@gmail.com>
2023-03-09 22:03:43 +0100

    fix mixins

--- src/main/java/com/cleanroommc/groovyscript/core/mixin/InventoryCraftingAccess.java
+++ src/main/java/com/cleanroommc/groovyscript/core/mixin/InventoryCraftingAccess.java
@@ -5,7 +5,7 @@ import net.minecraft.inventory.InventoryCrafting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(value = InventoryCrafting.class, remap = false)
+@Mixin(value = InventoryCrafting.class)
 public interface InventoryCraftingAccess {
 
     @Accessor
