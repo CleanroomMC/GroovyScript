da28224ea329f9a170f99815dde595b3d49e2366
Turing6 <39919441+Testure@users.noreply.github.com>
2023-05-19 12:21:16 -0500

    Base

---
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/forestry/Forestry.java
@@ -0,0 +1,10 @@
+package com.cleanroommc.groovyscript.compat.mods.forestry;
+
+import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
+
+public class Forestry extends ModPropertyContainer {
+
+    public Forestry() {
+
+    }
+}
