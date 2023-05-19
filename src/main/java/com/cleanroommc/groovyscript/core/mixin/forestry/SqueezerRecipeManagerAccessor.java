f91a11c7fe468d11e92410e1e204594b9674728f
Turing6 <39919441+Testure@users.noreply.github.com>
2023-05-19 12:21:40 -0500

    Squeezer

---
+++ src/main/java/com/cleanroommc/groovyscript/core/mixin/forestry/SqueezerRecipeManagerAccessor.java
@@ -0,0 +1,15 @@
+package com.cleanroommc.groovyscript.core.mixin.forestry;
+
+import forestry.api.recipes.ISqueezerRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Set;
+
+@Mixin(value = SqueezerRecipeManagerAccessor.class, remap = false)
+public interface SqueezerRecipeManagerAccessor {
+    @Accessor
+    static Set<ISqueezerRecipe> getRecipes() {
+        throw new AssertionError();
+    }
+}
