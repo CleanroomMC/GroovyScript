1045c89e09d2479ebb98b5030faadb614c4bc2cd
Turing6 <39919441+Testure@users.noreply.github.com>
2023-05-20 16:57:08 -0500

    fix accessor

--- src/main/java/com/cleanroommc/groovyscript/core/mixin/forestry/SqueezerRecipeManagerAccessor.java
+++ src/main/java/com/cleanroommc/groovyscript/core/mixin/forestry/SqueezerRecipeManagerAccessor.java
@@ -1,12 +1,13 @@
 package com.cleanroommc.groovyscript.core.mixin.forestry;
 
 import forestry.api.recipes.ISqueezerRecipe;
+import forestry.factory.recipes.SqueezerRecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Set;
 
-@Mixin(value = SqueezerRecipeManagerAccessor.class, remap = false)
+@Mixin(value = SqueezerRecipeManager.class, remap = false)
 public interface SqueezerRecipeManagerAccessor {
     @Accessor
     static Set<ISqueezerRecipe> getRecipes() {
