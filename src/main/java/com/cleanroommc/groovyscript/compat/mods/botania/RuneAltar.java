4fe2df3f0fe0be068d16f48133f2ff7ad98e49fb
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2023-07-24 21:28:17 -0700

    a horrid lack of a whitespace

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/botania/RuneAltar.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/botania/RuneAltar.java
@@ -66,7 +66,7 @@ public class RuneAltar extends VirtualizedRegistry<RecipeRuneAltar> {
     public boolean removeByInput(IIngredient... inputs) {
         List<Object> converted = Arrays.stream(inputs).map(i -> i instanceof OreDictIngredient ? ((OreDictIngredient) i).getOreDict() : i.getMatchingStacks()[0]).collect(Collectors.toList());
         if (BotaniaAPI.runeAltarRecipes.removeIf(recipe -> {
-            boolean found = converted.stream().allMatch(o -> recipe.getInputs().stream().anyMatch(i -> (i instanceof String || o instanceof String)? i.equals(o) : ItemStack.areItemStacksEqual((ItemStack) i, (ItemStack) o)));
+            boolean found = converted.stream().allMatch(o -> recipe.getInputs().stream().anyMatch(i -> (i instanceof String || o instanceof String) ? i.equals(o) : ItemStack.areItemStacksEqual((ItemStack) i, (ItemStack) o)));
             if (found) addBackup(recipe);
             return found;
         })) return true;
