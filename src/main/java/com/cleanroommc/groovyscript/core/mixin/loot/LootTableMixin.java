29aa50c28f8a003b911c53a814c60be5f4092306
Wong, Dylan <dylanwong@nevada.unr.edu>
2023-11-20 21:45:54 -0800

    this mixin class should be abstract to prevent instantiation

--- src/main/java/com/cleanroommc/groovyscript/core/mixin/loot/LootTableMixin.java
+++ src/main/java/com/cleanroommc/groovyscript/core/mixin/loot/LootTableMixin.java
@@ -6,11 +6,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(LootTable.class)
-public class LootTableMixin {
+public abstract class LootTableMixin {
 
     // dirty loot crash fix
-    @Inject(method = "freeze", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "freeze()V", at = @At("HEAD"), cancellable = true, remap = false)
     public void freeze(CallbackInfo ci) {
         ci.cancel();
     }
