ca26f65445606bfc35cc4ac3ebc56e91a6cf8d9d
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-02-18 23:27:38 -0800

    make enums == instead of equals

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/appliedenergistics2/Attunement.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/appliedenergistics2/Attunement.java
@@ -21,11 +21,11 @@ public class Attunement extends VirtualizedRegistry<Pair<Object, TunnelType>> {
     public void onReload() {
         removeScripted().forEach(pair -> {
             if (pair.getKey() instanceof ItemStack) {
-                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet().removeIf(x -> x.getKey().isItemEqual((ItemStack) pair.getKey()) && x.getValue().equals(pair.getValue()));
+                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet().removeIf(x -> x.getKey().isItemEqual((ItemStack) pair.getKey()) && x.getValue() == pair.getValue());
             } else if (pair.getKey() instanceof String) {
-                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet().removeIf(x -> x.getKey().equals(pair.getKey()) && x.getValue().equals(pair.getValue()));
+                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet().removeIf(x -> x.getKey().equals(pair.getKey()) && x.getValue() == pair.getValue());
             } else if (pair.getKey() instanceof Capability<?>) {
-                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet().removeIf(x -> x.getKey() == pair.getKey() && x.getValue().equals(pair.getValue()));
+                ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet().removeIf(x -> x.getKey() == pair.getKey() && x.getValue() == pair.getValue());
             }
         });
         restoreFromBackup().forEach(pair -> {
@@ -60,19 +60,19 @@ public class Attunement extends VirtualizedRegistry<Pair<Object, TunnelType>> {
     @MethodDescription(description = "groovyscript.wiki.appliedenergistics2.attunement.remove0", example = @Example("item('minecraft:lever'), tunnel('redstone')"))
     public void remove(ItemStack item, TunnelType tunnel) {
         addBackup(Pair.of(item, tunnel));
-        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet().removeIf(x -> x.getKey().isItemEqual(item) && x.getValue().equals(tunnel));
+        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet().removeIf(x -> x.getKey().isItemEqual(item) && x.getValue() == tunnel);
     }
 
     @MethodDescription(description = "groovyscript.wiki.appliedenergistics2.attunement.remove1", example = @Example("'thermaldynamics', tunnel('fe_power')"))
     public void remove(String modid, TunnelType tunnel) {
         addBackup(Pair.of(modid, tunnel));
-        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet().removeIf(x -> x.getKey().equals(modid) && x.getValue().equals(tunnel));
+        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet().removeIf(x -> x.getKey().equals(modid) && x.getValue() == tunnel);
     }
 
     @MethodDescription(description = "groovyscript.wiki.appliedenergistics2.attunement.remove2", example = @Example(value = "Capabilities.FORGE_ENERGY, tunnel('fe_power')", imports = "appeng.capabilities.Capabilities"))
     public void remove(Capability<?> capability, TunnelType tunnel) {
         addBackup(Pair.of(capability, tunnel));
-        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet().removeIf(x -> x.getKey() == capability && x.getValue().equals(tunnel));
+        ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet().removeIf(x -> x.getKey() == capability && x.getValue() == tunnel);
     }
 
     @MethodDescription
@@ -112,21 +112,21 @@ public class Attunement extends VirtualizedRegistry<Pair<Object, TunnelType>> {
     public void removeByTunnel(TunnelType tunnel) {
         for (Map.Entry<ItemStack, TunnelType> pair : ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet()
                 .stream()
-                .filter(x -> x.getValue().equals(tunnel))
+                .filter(x -> x.getValue() == tunnel)
                 .collect(Collectors.toList())) {
             addBackup(Pair.of(pair.getKey(), pair.getValue()));
             ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getTunnels().entrySet().removeIf(x -> x.getKey().isItemEqual(pair.getKey()));
         }
         for (Map.Entry<String, TunnelType> pair : ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet()
                 .stream()
-                .filter(x -> x.getValue().equals(tunnel))
+                .filter(x -> x.getValue() == tunnel)
                 .collect(Collectors.toList())) {
             addBackup(Pair.of(pair.getKey(), pair.getValue()));
             ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getModIdTunnels().entrySet().removeIf(x -> x.getKey().equals(pair.getKey()));
         }
         for (Map.Entry<Capability<?>, TunnelType> pair : ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet()
                 .stream()
-                .filter(x -> x.getValue().equals(tunnel))
+                .filter(x -> x.getValue() == tunnel)
                 .collect(Collectors.toList())) {
             addBackup(Pair.of(pair.getKey(), pair.getValue()));
             ((P2PTunnelRegistryAccessor) AEApi.instance().registries().p2pTunnel()).getCapTunnels().entrySet().removeIf(x -> x.getKey() == pair.getKey());
