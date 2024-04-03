604b6663f31274cc524c2ea0a2a0dae3b578d7c8
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-02 22:46:34 -0700

    clarify needsOverride javadoc

--- src/main/java/com/cleanroommc/groovyscript/api/documentation/annotations/Property.java
+++ src/main/java/com/cleanroommc/groovyscript/api/documentation/annotations/Property.java
@@ -123,7 +123,9 @@ public @interface Property {
 
     /**
      * Controls if the property needs an overriding property to enable it. Used in wrapper classes, such as {@link com.cleanroommc.groovyscript.helper.recipe.AbstractRecipeBuilder AbstractRecipeBuilderr}, where some or all of the fields
-     * may not be needed in subclasses. At least one property must have this element be {@code false} for the property to be documented.
+     * may not be needed in subclasses.
+     * This can also be used to effectively disable properties from being documented by attaching it to the lowest hierarchy.
+     * The annotation for the given property field with the lowest {@link #hierarchy} score must have this be {@code false} for the property to be documented.
      *
      * @return if the property needs an overriding annotation to enable it, defaults to {@code false}
      */
