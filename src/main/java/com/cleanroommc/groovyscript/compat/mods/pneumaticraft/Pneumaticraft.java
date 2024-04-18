881453813efd27aadd2a08ff1a344c82f269d413
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-18 06:16:52 -0700

    add pneumaticcraft

---
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/pneumaticraft/Pneumaticraft.java
@@ -0,0 +1,7 @@
+package com.cleanroommc.groovyscript.compat.mods.pneumaticraft;
+
+import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
+
+public class Pneumaticraft extends ModPropertyContainer {
+
+}
