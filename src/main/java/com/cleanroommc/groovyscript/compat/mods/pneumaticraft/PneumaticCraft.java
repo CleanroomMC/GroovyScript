0fd8b4b74fef8d7056f73fe24547b0810129a2fe
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-05-02 01:51:40 -0700

    rename pneu

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/pneumaticraft/PneumaticCraft.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/pneumaticraft/PneumaticCraft.java
@@ -2,6 +2,6 @@ package com.cleanroommc.groovyscript.compat.mods.pneumaticraft;
 
 import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
 
-public class Pneumaticraft extends ModPropertyContainer {
+public class PneumaticCraft extends ModPropertyContainer {
 
 }
