5c81c0146466313fd69df86157ed6005e1cb1323
brachy84 <brachy84@gmail.com>
2024-05-08 15:55:40 +0200

    more reviews

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/draconicevolution/DraconicEvolution.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/draconicevolution/DraconicEvolution.java
@@ -1,14 +1,9 @@
 package com.cleanroommc.groovyscript.compat.mods.draconicevolution;
 
-import com.cleanroommc.groovyscript.GroovyScriptConfig;
 import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
 
 public class DraconicEvolution extends ModPropertyContainer {
 
     public final Fusion fusion = new Fusion();
-    public final EnergyCore energyCore;
-
-    public DraconicEvolution() {
-        this.energyCore = GroovyScriptConfig.compat.draconicEvolutionEnergyCore ? new EnergyCore() : null;
-    }
+    public final EnergyCore energyCore = new EnergyCore();
 }
--- src/main/java/com/cleanroommc/groovyscript/compat/mods/draconicevolution/EnergyCore.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/draconicevolution/EnergyCore.java
@@ -1,5 +1,6 @@
 package com.cleanroommc.groovyscript.compat.mods.draconicevolution;
 
+import com.cleanroommc.groovyscript.GroovyScriptConfig;
 import com.cleanroommc.groovyscript.api.GroovyBlacklist;
 import com.cleanroommc.groovyscript.api.GroovyLog;
 import com.cleanroommc.groovyscript.api.IScriptReloadable;
@@ -38,6 +39,11 @@ public class EnergyCore implements IScriptReloadable {
         this.inner = new BlockStates[this.original.length];
         this.outer = new BlockStates[this.original.length];
         onReload(); // increases version to 1
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return GroovyScriptConfig.compat.draconicEvolutionEnergyCore;
     }
 
     @Override
