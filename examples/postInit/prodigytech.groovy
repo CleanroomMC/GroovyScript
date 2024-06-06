108b0626a404684759e7524aa6b3cfe86a46387d
Wizzerinus <development@toontowneventhorizon.com>
2024-06-06 15:36:42 +0300

    prodigytech: added the simple machines

--- examples/postInit/prodigytech.groovy
+++ examples/postInit/prodigytech.groovy
@@ -4,6 +4,102 @@
 
 println 'mod \'prodigytech\' detected, running script'
 
+// groovyscript.wiki.prodigytech.heat_sawmill.title:
+// groovyscript.wiki.prodigytech.heat_sawmill.description
+
+mods.prodigytech.heat_sawmill.removeByInput(ore('plankWood'))
+// mods.prodigytech.heat_sawmill.removeAll()
+
+mods.prodigytech.heat_sawmill.recipeBuilder()
+    .input(item('minecraft:gold_ingot'))
+    .output(item('minecraft:diamond'))
+    .time(50)
+    .register()
+
+mods.prodigytech.heat_sawmill.recipeBuilder()
+    .input(item('minecraft:iron_ingot'))
+    .output(item('minecraft:coal'))
+    .register()
+
+mods.prodigytech.heat_sawmill.recipeBuilder()
+    .input(item('minecraft:iron_block'))
+    .output(item('minecraft:emerald'), item('minecraft:clay'))
+    .register()
+
+mods.prodigytech.heat_sawmill.recipeBuilder()
+    .input(item('minecraft:gold_block'))
+    .output(item('minecraft:emerald'), item('minecraft:nether_star'))
+    .secondaryChance(0.25)
+    .time(50)
+    .register()
+
+
+// groovyscript.wiki.prodigytech.magnetic_reassembler.title:
+// groovyscript.wiki.prodigytech.magnetic_reassembler.description
+
+mods.prodigytech.magnetic_reassembler.removeByInput(item('minecraft:gravel'))
+// mods.prodigytech.magnetic_reassembler.removeAll()
+
+mods.prodigytech.magnetic_reassembler.recipeBuilder()
+    .input(item('minecraft:gold_ingot'))
+    .output(item('minecraft:diamond'))
+    .time(50)
+    .register()
+
+mods.prodigytech.magnetic_reassembler.recipeBuilder()
+    .input(item('minecraft:iron_ingot'))
+    .output(item('minecraft:coal'))
+    .register()
+
+
+// groovyscript.wiki.prodigytech.ore_refinery.title:
+// groovyscript.wiki.prodigytech.ore_refinery.description
+
+mods.prodigytech.ore_refinery.removeByInput(ore('oreLapis'))
+// mods.prodigytech.ore_refinery.removeAll()
+
+mods.prodigytech.ore_refinery.recipeBuilder()
+    .input(item('minecraft:gold_ingot'))
+    .output(item('minecraft:diamond'))
+    .time(50)
+    .register()
+
+mods.prodigytech.ore_refinery.recipeBuilder()
+    .input(item('minecraft:iron_ingot'))
+    .output(item('minecraft:coal'))
+    .register()
+
+mods.prodigytech.ore_refinery.recipeBuilder()
+    .input(item('minecraft:iron_block'))
+    .output(item('minecraft:emerald'), item('minecraft:clay'))
+    .register()
+
+mods.prodigytech.ore_refinery.recipeBuilder()
+    .input(item('minecraft:gold_block'))
+    .output(item('minecraft:emerald'), item('minecraft:nether_star'))
+    .secondaryChance(0.25)
+    .time(50)
+    .register()
+
+
+// groovyscript.wiki.prodigytech.rotary_grinder.title:
+// groovyscript.wiki.prodigytech.rotary_grinder.description
+
+mods.prodigytech.rotary_grinder.removeByInput(item('minecraft:gravel'))
+// mods.prodigytech.rotary_grinder.removeAll()
+
+mods.prodigytech.rotary_grinder.recipeBuilder()
+    .input(item('minecraft:gold_ingot'))
+    .output(item('minecraft:diamond'))
+    .time(50)
+    .register()
+
+mods.prodigytech.rotary_grinder.recipeBuilder()
+    .input(item('minecraft:iron_ingot'))
+    .output(item('minecraft:coal'))
+    .register()
+
+
 // groovyscript.wiki.prodigytech.solderer.title:
 // groovyscript.wiki.prodigytech.solderer.description
 
--- src/main/java/com/cleanroommc/groovyscript/compat/mods/prodigytech/ProdigyTech.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/prodigytech/ProdigyTech.java
@@ -3,21 +3,24 @@ package com.cleanroommc.groovyscript.compat.mods.prodigytech;
 import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
 
 public class ProdigyTech extends ModPropertyContainer {
-
     /*
     public final AtomicReshaper atomicReshaper = new AtomicReshaper();
     public final ExplosionFurnace explosionFurnace = new ExplosionFurnace();
-    public final HeatSawmill sawmill = new HeatSawmill();
-    public final MagneticReassember magneticReassember = new MagneticReassembler();
-    public final OreRefinery oreRefinery = new OreRefinery();
     public final PrimordialisReactor primordialisReactor = new PrimordialisReactor();
-    public final RotaryGrinder rotaryGrinder = new RotaryGrinder();
      */
+    public final SimpleRecipeHandler magneticReassembler = new MagneticReassembler();
+    public final SimpleRecipeHandlerSecondaryOutput oreRefinery = new OreRefinery();
+    public final SimpleRecipeHandler grinder = new RotaryGrinder();
+    public final SimpleRecipeHandlerSecondaryOutput sawmill = new HeatSawmill();
     public final Solderer solderer = new Solderer();
     public final ZorraAltar zorraAltar = new ZorraAltar();
     public final ZorraAltarItem zorraAltarItem = new ZorraAltarItem();
 
     public ProdigyTech() {
+        addRegistry(magneticReassembler);
+        addRegistry(oreRefinery);
+        addRegistry(grinder);
+        addRegistry(sawmill);
         addRegistry(solderer);
         addRegistry(zorraAltar);
         addRegistry(zorraAltarItem);
