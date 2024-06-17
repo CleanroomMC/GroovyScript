790001beb4a65b1c34cf3388e286f7a81e620322
brachy84 <brachy84@gmail.com>
2024-06-17 21:51:39 +0200

    fix backwards compat

--- src/main/java/com/cleanroommc/groovyscript/api/IGameObjectParser.java
+++ src/main/java/com/cleanroommc/groovyscript/api/IGameObjectParser.java
@@ -1,10 +1,15 @@
 package com.cleanroommc.groovyscript.api;
 
+import com.cleanroommc.groovyscript.mapper.ObjectMappers;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
@@ -15,27 +20,51 @@ import java.util.function.Function;
 @FunctionalInterface
 public interface IGameObjectParser<T> extends IObjectParser<T> {
 
-    static <T extends IForgeRegistryEntry<T>> IObjectParser<T> wrapForgeRegistry(IForgeRegistry<T> forgeRegistry) {
-        return IObjectParser.wrapForgeRegistry(forgeRegistry);
+    static <T extends IForgeRegistryEntry<T>> IGameObjectParser<T> wrapForgeRegistry(IForgeRegistry<T> forgeRegistry) {
+        return (s, args) -> {
+            Result<ResourceLocation> rl = ObjectMappers.parseResourceLocation(s, args);
+            if (rl.hasError()) return Result.error(rl.getError());
+            T value = forgeRegistry.getValue(rl.getValue());
+            return value == null ? Result.error() : Result.some(value);
+        };
     }
 
-    static <T extends Enum<T>> IObjectParser<T> wrapEnum(Class<T> enumClass, boolean caseSensitive) {
-        return IObjectParser.wrapEnum(enumClass, caseSensitive);
+    static <T extends Enum<T>> IGameObjectParser<T> wrapEnum(Class<T> enumClass, boolean caseSensitive) {
+        Map<String, T> map = new Object2ObjectOpenHashMap<>();
+        for (T t : enumClass.getEnumConstants()) {
+            map.put(caseSensitive ? t.name() : t.name().toUpperCase(Locale.ROOT), t);
+        }
+        return (s, args) -> {
+            T t = map.get(caseSensitive ? s : s.toUpperCase(Locale.ROOT));
+            return t == null ? Result.error() : Result.some(t);
+        };
     }
 
-    static <T> IObjectParser<T> wrapStringGetter(Function<String, T> getter) {
-        return IObjectParser.wrapStringGetter(getter);
+    static <T> IGameObjectParser<T> wrapStringGetter(Function<String, T> getter) {
+        return wrapStringGetter(getter, false);
     }
 
-    static <T> IObjectParser<T> wrapStringGetter(Function<String, T> getter, boolean isUpperCase) {
-        return IObjectParser.wrapStringGetter(getter, isUpperCase);
+    static <T> IGameObjectParser<T> wrapStringGetter(Function<String, T> getter, boolean isUpperCase) {
+        return (s, args) -> {
+            if (args.length > 0) {
+                return Result.error("extra arguments are not allowed");
+            }
+            T t = getter.apply(isUpperCase ? s.toUpperCase(Locale.ROOT) : s);
+            return t == null ? Result.error() : Result.some(t);
+        };
     }
 
-    static <T, V> IObjectParser<T> wrapStringGetter(Function<String, V> getter, Function<V, @NotNull T> trueTypeFunction) {
-        return IObjectParser.wrapStringGetter(getter, trueTypeFunction);
+    static <T, V> IGameObjectParser<T> wrapStringGetter(Function<String, V> getter, Function<V, @NotNull T> trueTypeFunction) {
+        return wrapStringGetter(getter, trueTypeFunction, false);
     }
 
-    static <T, V> IObjectParser<T> wrapStringGetter(Function<String, V> getter, Function<V, @NotNull T> trueTypeFunction, boolean isUpperCase) {
-        return IObjectParser.wrapStringGetter(getter, trueTypeFunction, isUpperCase);
+    static <T, V> IGameObjectParser<T> wrapStringGetter(Function<String, V> getter, Function<V, @NotNull T> trueTypeFunction, boolean isUpperCase) {
+        return (s, args) -> {
+            if (args.length > 0) {
+                return Result.error("extra arguments are not allowed");
+            }
+            V v = getter.apply(isUpperCase ? s.toUpperCase(Locale.ROOT) : s);
+            return v == null ? Result.error() : Result.some(trueTypeFunction.apply(v));
+        };
     }
 }
--- src/main/java/com/cleanroommc/groovyscript/gameobjects/Completer.java
+++ src/main/java/com/cleanroommc/groovyscript/gameobjects/Completer.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.groovyscript.gameobjects;
 
 import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionItemKind;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.function.Function;
@@ -12,10 +13,29 @@ import java.util.function.Supplier;
 public interface Completer extends com.cleanroommc.groovyscript.mapper.Completer {
 
     static <V> Completer ofNamed(Supplier<Iterable<V>> values, Function<V, String> toString, int preferredParamIndex) {
-        return (Completer) com.cleanroommc.groovyscript.mapper.Completer.ofNamed(values, toString, preferredParamIndex);
+        return ofValues(values, v -> {
+            String s = toString.apply(v);
+            if (s != null) {
+                var item = new CompletionItem(toString.apply(v));
+                item.setKind(CompletionItemKind.Constant);
+                return item;
+            }
+            return null;
+        }, preferredParamIndex);
     }
 
     static <V> Completer ofValues(Supplier<Iterable<V>> values, Function<V, CompletionItem> toCompletionItem, int preferredParamIndex) {
-        return (Completer) com.cleanroommc.groovyscript.mapper.Completer.ofValues(values, toCompletionItem, preferredParamIndex);
+        return (paramIndex, items) -> {
+            if (preferredParamIndex < 0 || preferredParamIndex == paramIndex) {
+                items.addAll(values.get(), toCompletionItem);
+            }
+        };
+    }
+
+    default Completer and(com.cleanroommc.groovyscript.mapper.Completer other) {
+        return (paramIndex, items) -> {
+            complete(paramIndex, items);
+            other.complete(paramIndex, items);
+        };
     }
 }
