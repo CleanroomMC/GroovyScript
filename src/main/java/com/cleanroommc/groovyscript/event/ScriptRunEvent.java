38e20457d1d7e3bfe84f045cc107470e4582459f
brachy84 <brachy84@gmail.com>
2024-07-02 21:31:53 +0200

    add load stage field to ScriptRunEvent

--- src/main/java/com/cleanroommc/groovyscript/event/ScriptRunEvent.java
+++ src/main/java/com/cleanroommc/groovyscript/event/ScriptRunEvent.java
@@ -1,18 +1,37 @@
 package com.cleanroommc.groovyscript.event;
 
+import com.cleanroommc.groovyscript.sandbox.LoadStage;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 public class ScriptRunEvent extends Event {
+
+    private final LoadStage loadStage;
+
+    public ScriptRunEvent(LoadStage loadStage) {
+        this.loadStage = loadStage;
+    }
+
+    public LoadStage getLoadStage() {
+        return loadStage;
+    }
 
     /**
      * Called before anything on script run (first load and reload)
      */
     public static class Pre extends ScriptRunEvent {
+
+        public Pre(LoadStage loadStage) {
+            super(loadStage);
+        }
     }
 
     /**
      * Called after anything on script run (first load and reload)
      */
     public static class Post extends ScriptRunEvent {
+
+        public Post(LoadStage loadStage) {
+            super(loadStage);
+        }
     }
 }
