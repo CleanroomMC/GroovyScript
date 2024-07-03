b04b04c3b46415dad63fc80df3d272d76951a347
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-07-03 16:03:42 -0700

    update ModPropertyContainer

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/immersivepetroleum/ImmersivePetroleum.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/immersivepetroleum/ImmersivePetroleum.java
@@ -1,15 +1,10 @@
 package com.cleanroommc.groovyscript.compat.mods.immersivepetroleum;
 
-import com.cleanroommc.groovyscript.compat.mods.ModPropertyContainer;
+import com.cleanroommc.groovyscript.compat.mods.GroovyPropertyContainer;
 
-public class ImmersivePetroleum extends ModPropertyContainer {
+public class ImmersivePetroleum extends GroovyPropertyContainer {
 
     public final Distillation distillation = new Distillation();
     public final Reservoir reservoir = new Reservoir();
-
-    public ImmersivePetroleum() {
-        addRegistry(distillation);
-        addRegistry(reservoir);
-    }
 
 }
