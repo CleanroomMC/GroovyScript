607aa2015cfcb613bdaf6244869c5f3fa7dcde8d
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-07-04 19:33:33 -0700

    fix mekanism asGroovyCode infusionType

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/mekanism/Mekanism.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/mekanism/Mekanism.java
@@ -63,7 +63,7 @@ public class Mekanism extends GroovyPropertyContainer {
 
     @Optional.Method(modid = "mekanism")
     public static String asGroovyCode(InfuseType infuseType, boolean colored) {
-        return GroovyScriptCodeConverter.formatGenericHandler("infusion", infuseType.unlocalizedName, colored);
+        return GroovyScriptCodeConverter.formatGenericHandler("infusionType", infuseType.unlocalizedName, colored);
     }
 
     @Optional.Method(modid = "mekanism")
