b9e3e85647edf770b0dbb58688f81c5343624529
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-07-04 18:49:53 -0700

    add multiple info commands and info parser registry

---
+++ src/main/java/com/cleanroommc/groovyscript/api/infocommand/InfoParserPackage.java
@@ -0,0 +1,181 @@
+package com.cleanroommc.groovyscript.api.infocommand;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Created when /gs info is executed
+ */
+public class InfoParserPackage {
+
+    /**
+     * Server where the command is executed
+     */
+    @NotNull
+    private final MinecraftServer server;
+    /**
+     * Player who executes the command
+     */
+    @NotNull
+    private final EntityPlayer player;
+    /**
+     * Arguments of the command
+     */
+    @NotNull
+    private final List<String> args;
+    /**
+     * A list of messages that will be sent to the player after this event.
+     * Add or remove your messages here.
+     */
+    @NotNull
+    private final List<ITextComponent> messages;
+    /**
+     * If pretty nbt is enabled
+     */
+    private final boolean prettyNbt;
+    /**
+     * The held item or the item form of the block being looked at.
+     */
+    @NotNull
+    private ItemStack stack;
+    /**
+     * The entity the player is looking at
+     */
+    @Nullable
+    private Entity entity;
+    /**
+     * The block position the player is looking at
+     */
+    @Nullable
+    private BlockPos pos;
+    /**
+     * The block state of the held item or the block state the player is looking at
+     */
+    @Nullable
+    private IBlockState blockState;
+    /**
+     * The block of the held item or the block the player is looking at
+     */
+    @Nullable
+    private Block block;
+    /**
+     * The tile entity the player is looking at
+     */
+    @Nullable
+    private TileEntity tileEntity;
+
+    public InfoParserPackage(
+            @NotNull MinecraftServer server,
+            @NotNull EntityPlayer player,
+            @NotNull List<String> args,
+            @NotNull List<ITextComponent> messages,
+            boolean prettyNbt
+    ) {
+        this.server = server;
+        this.player = player;
+        this.args = args;
+        this.messages = messages;
+        this.stack = ItemStack.EMPTY;
+        this.prettyNbt = prettyNbt;
+    }
+
+    public @NotNull MinecraftServer getServer() {
+        return server;
+    }
+
+    public @NotNull EntityPlayer getPlayer() {
+        return player;
+    }
+
+    public @NotNull List<String> getArgs() {
+        return args;
+    }
+
+    public @NotNull List<ITextComponent> getMessages() {
+        return messages;
+    }
+
+    public @NotNull ItemStack getStack() {
+        return stack;
+    }
+
+    public void setStack(@NotNull ItemStack stack) {
+        this.stack = stack;
+    }
+
+    public boolean isPrettyNbt() {
+        return prettyNbt;
+    }
+
+    public @Nullable Entity getEntity() {
+        return entity;
+    }
+
+    public void setEntity(@Nullable Entity entity) {
+        this.entity = entity;
+    }
+
+    public @Nullable BlockPos getPos() {
+        return pos;
+    }
+
+    public void setPos(@Nullable BlockPos pos) {
+        this.pos = pos;
+    }
+
+    public @Nullable IBlockState getBlockState() {
+        return blockState;
+    }
+
+    public void setBlockState(@Nullable IBlockState blockState) {
+        this.blockState = blockState;
+    }
+
+    public @Nullable Block getBlock() {
+        return block;
+    }
+
+    public void setBlock(@Nullable Block block) {
+        this.block = block;
+    }
+
+    public @Nullable TileEntity getTileEntity() {
+        return tileEntity;
+    }
+
+    public void setTileEntity(@Nullable TileEntity tileEntity) {
+        this.tileEntity = tileEntity;
+    }
+
+    public void copyFromPos(BlockPos pos) {
+        if (pos == null) return;
+        this.pos = pos;
+        this.blockState = player.world.getBlockState(pos);
+        this.block = blockState.getBlock();
+        this.tileEntity = player.world.getTileEntity(pos);
+
+        this.stack = block.getPickBlock(blockState, Minecraft.getMinecraft().objectMouseOver, player.world, pos, player);
+        if (this.stack.isEmpty()) this.stack = new ItemStack(block, 1, block.getMetaFromState(blockState));
+    }
+
+    public void parse() {
+        parse(false);
+    }
+
+    public void parse(boolean force) {
+        InfoParserRegistry.getInfoParsers().forEach(x -> x.parse(this, force));
+    }
+
+}
---
+++ src/main/java/com/cleanroommc/groovyscript/command/BaseInfoCommand.java
@@ -0,0 +1,138 @@
+package com.cleanroommc.groovyscript.command;
+
+import com.cleanroommc.groovyscript.api.infocommand.InfoParserPackage;
+import com.cleanroommc.groovyscript.api.infocommand.InfoParserRegistry;
+import com.google.common.base.Predicates;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class BaseInfoCommand extends CommandBase {
+
+    /**
+     * gets the block being looked at, stopping on fluid blocks
+     */
+    protected static BlockPos getBlockLookingAt(EntityPlayer player) {
+        double distance = player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue();
+        Vec3d eyes = player.getPositionEyes(0.0F);
+        Vec3d look = player.getLook(0.0F);
+        Vec3d end = eyes.add(look.x * distance, look.y * distance, look.z * distance);
+
+        RayTraceResult result = player.getEntityWorld().rayTraceBlocks(eyes, end, true);
+        if (result != null && result.typeOfHit == RayTraceResult.Type.BLOCK) {
+            return result.getBlockPos();
+        }
+        return null;
+    }
+
+    /**
+     * gets the closest entity being looked at
+     */
+    protected static Entity getEntityLookingAt(EntityPlayer player) {
+        Entity entity = null;
+        double d0 = 0.0D;
+
+        double distance = player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue();
+        Vec3d eyes = player.getPositionEyes(0.0F);
+        Vec3d look = player.getLook(0.0F);
+        Vec3d end = eyes.add(look.x * distance, look.y * distance, look.z * distance);
+
+        List<Entity> list = player.world.getEntitiesInAABBexcluding(
+                player,
+                player.getEntityBoundingBox()
+                        .expand(look.x * distance, look.y * distance, look.z * distance)
+                        .grow(1.0D, 1.0D, 1.0D),
+                Predicates.and(EntitySelectors.NOT_SPECTATING, e -> e != null && e.canBeCollidedWith()));
+
+        for (Entity entity1 : list) {
+            AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().grow(0.3);
+            RayTraceResult raytraceresult = axisalignedbb.calculateIntercept(eyes, end);
+
+            if (raytraceresult != null) {
+                double d1 = eyes.squareDistanceTo(raytraceresult.hitVec);
+                if (d1 < d0 || d0 == 0.0D) {
+                    entity = entity1;
+                    d0 = d1;
+                }
+            }
+        }
+        return entity;
+    }
+
+    @Override
+    public @NotNull String getUsage(@NotNull ICommandSender sender) {
+        return String.format("/gs %s [all, pretty, %s]", getName(), String.join(", ", InfoParserRegistry.getIds()));
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+
+    @Override
+    public @NotNull List<String> getTabCompletions(@NotNull MinecraftServer server, @NotNull ICommandSender sender, String @NotNull [] args,
+                                                   @Nullable BlockPos targetPos) {
+        List<String> enabledModes = new ArrayList<>(InfoParserRegistry.getIds());
+        enabledModes.add("all");
+        enabledModes.add("pretty");
+        Arrays.stream(args).map(String::toLowerCase).forEach(enabledModes::remove);
+        return getListOfStringsMatchingLastWord(args, enabledModes);
+    }
+
+    abstract String targetDescription();
+
+    protected void print(EntityPlayer player, List<ITextComponent> messages, List<String> argList) {
+        if (messages.isEmpty()) {
+            player.sendMessage(new TextComponentString(String.format("Couldn't find %s!", targetDescription())).setStyle(new Style().setColor(TextFormatting.RED)));
+            if (!argList.isEmpty()) player.sendMessage(new TextComponentString("The following arguments were provided: " + String.join(", ", argList)));
+        } else {
+            // have a horizontal bar to improve readability when running multiple consecutive info commands
+            player.sendMessage(new TextComponentString("================================").setStyle(new Style().setColor(TextFormatting.GOLD)));
+            messages.forEach(player::sendMessage);
+        }
+    }
+
+    abstract void gatherInfo(InfoParserPackage info, EntityPlayer player);
+
+    @Override
+    public void execute(@NotNull MinecraftServer server, @NotNull ICommandSender sender, String @NotNull [] args) {
+        if (sender instanceof EntityPlayer player) {
+            List<ITextComponent> messages = new ArrayList<>();
+
+            // get all distinct arguments
+            List<String> argList = Arrays.stream(args).distinct().collect(Collectors.toList());
+            // remove "pretty" from the list so that it is ignored for determining if force is true
+            boolean prettyNbt = argList.remove("pretty");
+
+            // if there are 0 args or the args contain "all", we want to print every option
+            boolean force = argList.contains("all") || args.length == 0;
+
+            InfoParserPackage info = new InfoParserPackage(server, player, argList, messages, prettyNbt);
+
+            // add different data to the info parser depending on the command being used
+            gatherInfo(info, player);
+
+            info.parse(force);
+            print(player, messages, argList);
+        }
+    }
+
+}
