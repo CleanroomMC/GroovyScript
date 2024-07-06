0a9cdde0e4673fccfdd6ccf7ba7c7a4ff72248d0
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-07-05 23:03:54 -0700

    add Immersive Petroleum compat (#171)
    * add Immersive Petroleum compat
    
    * remove incorrect annotation
    
    * convert distillation to use different method of adding chance
    
    * update ModPropertyContainer

---
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/immersivepetroleum/ImmersivePetroleum.java
@@ -0,0 +1,10 @@
+package com.cleanroommc.groovyscript.compat.mods.immersivepetroleum;
+
+import com.cleanroommc.groovyscript.compat.mods.GroovyPropertyContainer;
+
+public class ImmersivePetroleum extends GroovyPropertyContainer {
+
+    public final Distillation distillation = new Distillation();
+    public final Reservoir reservoir = new Reservoir();
+
+}
