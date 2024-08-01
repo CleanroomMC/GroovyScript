19c53f6f6659e4b489a2c1d6cf2c30532eed7149
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-07-31 23:31:40 -0700

    remove unused accessor method

--- src/main/java/com/cleanroommc/groovyscript/core/mixin/jei/RecipeLayoutAccessor.java
+++ src/main/java/com/cleanroommc/groovyscript/core/mixin/jei/RecipeLayoutAccessor.java
@@ -1,19 +1,12 @@
 package com.cleanroommc.groovyscript.core.mixin.jei;
 
-import mezz.jei.api.gui.IGuiIngredientGroup;
-import mezz.jei.api.recipe.IIngredientType;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.gui.recipes.RecipeLayout;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.Map;
-
 @Mixin(value = RecipeLayout.class, remap = false)
 public interface RecipeLayoutAccessor {
-
-    @Accessor("guiIngredientGroups")
-    Map<IIngredientType, IGuiIngredientGroup> getGuiIngredientGroups();
 
     @Accessor("recipeWrapper")
     IRecipeWrapper getRecipeWrapper();
