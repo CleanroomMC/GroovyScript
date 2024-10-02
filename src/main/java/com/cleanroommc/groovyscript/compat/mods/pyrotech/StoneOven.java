85ac8221949032b693c20cc53ce086289457b32f
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-10-02 02:58:26 -0700

    convert deprecated elements of recent pyrotech

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/pyrotech/StoneOven.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/pyrotech/StoneOven.java
@@ -68,12 +68,12 @@ public class StoneOven extends ForgeRegistryWrapper<StoneOvenRecipe> {
         }
     }
 
-    @Property(property = "input", valid = @Comp("1"))
-    @Property(property = "output", valid = @Comp("1"))
+    @Property(property = "input", comp = @Comp(eq = 1))
+    @Property(property = "output", comp = @Comp(eq = 1))
     @Property(property = "name")
     public static class RecipeBuilder extends AbstractRecipeBuilder<StoneOvenRecipe> {
 
-        @Property(valid = @Comp(type = Comp.Type.GTE, value = "1"))
+        @Property(comp = @Comp(gte = 1))
         private int duration;
 
         @RecipeBuilderMethodDescription
