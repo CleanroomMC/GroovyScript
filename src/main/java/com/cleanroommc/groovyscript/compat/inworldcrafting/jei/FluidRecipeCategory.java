abab05d14aad4f32a0175ccd1c48cabdd5f5a3a4
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-12-02 23:11:35 -0800

    use canonical forms of floats

--- src/main/java/com/cleanroommc/groovyscript/compat/inworldcrafting/jei/FluidRecipeCategory.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/inworldcrafting/jei/FluidRecipeCategory.java
@@ -68,7 +68,7 @@ public class FluidRecipeCategory extends BaseCategory<FluidRecipeCategory.Recipe
         recipeLayout.getItemStacks().addTooltipCallback((slotIndex, input, ingredient, tooltip) -> {
             if (slotIndex < 9) {
                 float chance = recipeWrapper.recipe.getItemConsumeChance()[slotIndex];
-                if (chance < 1f) {
+                if (chance < 1.0f) {
                     tooltip.add(1, I18n.format("groovyscript.recipe.chance_consume", numberFormat.format(chance)));
                 }
             }
@@ -76,7 +76,7 @@ public class FluidRecipeCategory extends BaseCategory<FluidRecipeCategory.Recipe
         recipeLayout.getFluidStacks().addTooltipCallback((slotIndex, input, ingredient, tooltip) -> {
             if (slotIndex == 10 && recipeWrapper.recipe instanceof FluidToItem.Recipe fluidToItemRecipe) {
                 float chance = fluidToItemRecipe.getFluidConsumptionChance();
-                if (chance < 1f) {
+                if (chance < 1.0f) {
                     tooltip.add(1, I18n.format("groovyscript.recipe.chance_consume", numberFormat.format(chance)));
                 }
             }
