8f33d893cafae88e023de696562f5abfb690a3d0
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-12-06 19:37:35 -0800

    enhance description and add todo for IOreDicts

--- src/main/java/com/cleanroommc/groovyscript/api/IOreDicts.java
+++ src/main/java/com/cleanroommc/groovyscript/api/IOreDicts.java
@@ -2,8 +2,18 @@ package com.cleanroommc.groovyscript.api;
 
 import java.util.List;
 
-public interface IOreDicts {
+/**
+ * Indicates that the IIngredient represents one or more oredicts.
+ */
+public interface IOreDicts extends IIngredient {
+    // TODO
+    //  There are a large number of places currently in the GroovyScript codebase
+    //  that check if something is "instanceof OreDictIngredient".
+    //  these should be replaced by checks against IOreDicts,
+    //  and surrounding code changed appropriately.
 
+    /**
+     * @return a list of oredict strings
+     */
     List<String> getOreDicts();
-
 }
