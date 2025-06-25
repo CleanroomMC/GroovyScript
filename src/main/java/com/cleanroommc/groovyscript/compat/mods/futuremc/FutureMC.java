86f371fd7a8516489553e70beb7828e327498ad2
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-06-25 09:05:11 -0700

    add FutureMC Composter compat (#308)
    * add composter compat
    
    * make bonemeal invalid
    
    * 8 layers not 7, whoops

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/futuremc/FutureMC.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/futuremc/FutureMC.java
@@ -6,6 +6,7 @@ public class FutureMC extends GroovyPropertyContainer {
 
     public final BlastFurnace blastFurnace = new BlastFurnace();
     public final Campfire campfire = new Campfire();
+    public final Composter composter = new Composter();
     public final Smithing smithing = new Smithing();
     public final Smoker smoker = new Smoker();
     public final Stonecutter stonecutter = new Stonecutter();
