56e706263e50a2943015db547b871489cbe92845
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-08-28 08:34:40 -0700

    convert vanilla global binding to list

--- src/main/java/com/cleanroommc/groovyscript/compat/vanilla/VanillaModule.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/vanilla/VanillaModule.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.groovyscript.compat.vanilla;
 
 import com.cleanroommc.groovyscript.GroovyScript;
+import com.cleanroommc.groovyscript.api.INamed;
 import com.cleanroommc.groovyscript.compat.content.Content;
 import com.cleanroommc.groovyscript.compat.inworldcrafting.InWorldCrafting;
 import com.cleanroommc.groovyscript.compat.loot.Loot;
@@ -9,6 +10,9 @@ import com.cleanroommc.groovyscript.compat.mods.GroovyPropertyContainer;
 import com.cleanroommc.groovyscript.sandbox.expand.ExpansionHelper;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VanillaModule extends GroovyPropertyContainer {
 
@@ -25,23 +29,30 @@ public class VanillaModule extends GroovyPropertyContainer {
     public static final Command command = new Command();
     public static final GameRule gameRule = new GameRule();
 
-    private VanillaModule() {}
+    private final List<INamed> globalBindings = new ArrayList<>();
+
+    private VanillaModule() {
+        // maybe remove some of these as globals?
+        globalBindings.add(crafting);
+        globalBindings.add(furnace);
+        globalBindings.add(loot);
+        globalBindings.add(oreDict);
+        globalBindings.add(player);
+        globalBindings.add(content);
+        globalBindings.add(rarity);
+        globalBindings.add(inWorldCrafting);
+        globalBindings.add(command);
+        globalBindings.add(gameRule);
+    }
 
     @Override
     public void initialize(GroovyContainer<?> owner) {
         GroovyScript.getSandbox().registerBinding("Minecraft", this);
         GroovyScript.getSandbox().registerBinding("Vanilla", this);
-        // maybe remove some of these as globals?
-        GroovyScript.getSandbox().registerBinding(crafting);
-        GroovyScript.getSandbox().registerBinding(furnace);
-        GroovyScript.getSandbox().registerBinding(loot);
-        GroovyScript.getSandbox().registerBinding(oreDict);
-        GroovyScript.getSandbox().registerBinding(player);
-        GroovyScript.getSandbox().registerBinding(content);
-        GroovyScript.getSandbox().registerBinding(rarity);
-        GroovyScript.getSandbox().registerBinding(inWorldCrafting);
-        GroovyScript.getSandbox().registerBinding(command);
-        GroovyScript.getSandbox().registerBinding(gameRule);
+
+        for (INamed registry : globalBindings) {
+            GroovyScript.getSandbox().registerBinding(registry);
+        }
 
         ExpansionHelper.mixinClass(ItemStack.class, ItemStackExpansion.class);
         ExpansionHelper.mixinClass(ICommandSender.class, CommandSenderExpansion.class);
