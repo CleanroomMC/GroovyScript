11959978e79002a6b31fce57caf0243b41fb937b
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-08-28 09:13:33 -0700

    remove player global binding

--- src/main/java/com/cleanroommc/groovyscript/compat/vanilla/VanillaModule.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/vanilla/VanillaModule.java
@@ -35,7 +35,6 @@ public class VanillaModule extends GroovyPropertyContainer {
         globalBindings.add(crafting);
         globalBindings.add(furnace);
         globalBindings.add(oreDict);
-        globalBindings.add(player);
         globalBindings.add(content);
         globalBindings.add(inWorldCrafting);
     }
