442f9f3092c5a423b80dbf36e000313bc3fd5a71
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-10-04 14:43:06 -0700

    tinkers construct

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/tinkersconstruct/TinkersConstruct.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/tinkersconstruct/TinkersConstruct.java
@@ -8,6 +8,7 @@ import com.cleanroommc.groovyscript.compat.mods.tinkersconstruct.material.Materi
 import com.cleanroommc.groovyscript.compat.mods.tinkersconstruct.material.ToolMaterialBuilder;
 import com.cleanroommc.groovyscript.compat.mods.tinkersconstruct.material.traits.TraitRegistryEvent;
 import com.cleanroommc.groovyscript.core.mixin.tconstruct.TinkerRegistryAccessor;
+import com.cleanroommc.groovyscript.helper.ingredient.GroovyScriptCodeConverter;
 import net.minecraftforge.common.MinecraftForge;
 import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.traits.ITrait;
@@ -23,17 +24,35 @@ public class TinkersConstruct extends GroovyPropertyContainer {
     public final CastingBasin castingBasin = new CastingBasin();
     public final Materials materials = new Materials();
 
+    public static String asGroovyCode(Material material, boolean colored) {
+        return GroovyScriptCodeConverter.formatGenericHandler("material", material.identifier, colored);
+    }
+
+    public static String asGroovyCode(String handler, ITrait trait, boolean colored) {
+        return GroovyScriptCodeConverter.formatGenericHandler(handler, trait.getIdentifier(), colored);
+    }
+
+    public static String asGroovyCodeTool(ITrait trait, boolean colored) {
+        return asGroovyCode("toolTrait", trait, colored);
+    }
+
+    public static String asGroovyCodeArmor(ITrait trait, boolean colored) {
+        return asGroovyCode("armorTrait", trait, colored);
+    }
+
     @Override
     public void initialize(GroovyContainer<?> container) {
         container.objectMapperBuilder("toolMaterial", Material.class)
                 .parser(IObjectParser.wrapStringGetter(TinkerRegistryAccessor.getMaterials()::get))
                 .completerOfNames(TinkerRegistryAccessor.getMaterials()::keySet)
                 .docOfType("tool material")
+                .toGroovyCode(x -> asGroovyCode(x, false))
                 .register();
         container.objectMapperBuilder("toolTrait", ITrait.class)
                 .parser(IObjectParser.wrapStringGetter(TinkerRegistryAccessor.getTraits()::get))
                 .completerOfNamed(TinkerRegistryAccessor.getTraits()::keySet, v -> v.endsWith("_armor") ? null : v) // only suggest non armor traits
                 .docOfType("tool trait")
+                .toGroovyCode(x -> asGroovyCodeTool(x, false))
                 .register();
         container.objectMapperBuilder("armorTrait", ITrait.class)
                 .parser(IObjectParser.wrapStringGetter(s -> TinkerRegistryAccessor.getTraits().get(s + "_armor")))
@@ -41,6 +60,7 @@ public class TinkersConstruct extends GroovyPropertyContainer {
                         TinkerRegistryAccessor.getTraits()::keySet,
                         v -> v.endsWith("_armor") ? v.substring(0, v.length() - 6) : null) // only suggest armor traits
                 .docOfType("armor trait")
+                .toGroovyCode(x -> asGroovyCodeArmor(x, false))
                 .register();
     }
 
