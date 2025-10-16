814c748da972f1f173431d1544e79070cee93fd7
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-10-16 03:48:53 -0700

    adjust vanilla logic with custom container

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/MinecraftModContainer.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/MinecraftModContainer.java
@@ -1,19 +1,17 @@
 package com.cleanroommc.groovyscript.compat.mods;
 
-import com.cleanroommc.groovyscript.GroovyScript;
 import com.cleanroommc.groovyscript.api.documentation.IContainerDocumentation;
 import com.cleanroommc.groovyscript.compat.vanilla.VanillaModule;
 import com.cleanroommc.groovyscript.documentation.Documentation;
 import com.cleanroommc.groovyscript.documentation.Exporter;
 import com.cleanroommc.groovyscript.documentation.helper.ContainerHolder;
+import com.cleanroommc.groovyscript.sandbox.LoadStage;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
@@ -67,16 +65,22 @@ public final class MinecraftModContainer extends GroovyContainer<VanillaModule> 
     @Override
     public void onCompatLoaded(GroovyContainer<?> container) {}
 
+    private ContainerHolder getContainer() {
+        var aliases = ContainerHolder.expandAliases(getAliases());
+        aliases.addAll(getAliases());
+        return new ContainerHolder(getModId(), getContainerName(), getModId(), aliases, new ObjectOpenHashSet<>(get().getRegistries()));
+    }
+
+    @Override
+    public boolean generateExamples(File suggestedFile, LoadStage stage) {
+        Exporter.generateExamples(suggestedFile, stage, getContainer());
+        return false;
+    }
+
     @Override
     public boolean generateWiki(File suggestedFolder) {
-        var minecraftCompatFolder = new File(new File(Documentation.WIKI, "minecraft"), "helpers");
-        try {
-            Files.createDirectories(minecraftCompatFolder.toPath());
-            Exporter.generateWiki(minecraftCompatFolder, ContainerHolder.of(this));
-            Exporter.exportFile(new File(minecraftCompatFolder, "index.md"), "wiki/vanilla/index.md");
-        } catch (IOException e) {
-            GroovyScript.LOGGER.throwing(e);
-        }
+        var minecraftCompatFolder = new File(Documentation.WIKI_MINECRAFT, "helpers");
+        Exporter.generateWiki(minecraftCompatFolder, getContainer());
         return false;
     }
 }
