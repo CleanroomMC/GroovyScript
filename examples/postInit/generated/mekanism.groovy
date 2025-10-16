3e5ceca715df240968c0cf3ceecd1f8096428cd3
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-10-16 04:01:19 -0700

    generate mekanism infusion texture

--- examples/postInit/generated/mekanism.groovy
+++ examples/postInit/generated/mekanism.groovy
@@ -12,7 +12,7 @@ mods.mekanism.infusion.removeByType(infusionType('carbon'))
 // mods.mekanism.infusion.removeByType(infusionType('diamond'))
 // mods.mekanism.infusion.removeAll()
 
-mods.mekanism.infusion.addType('groovy_example', resource('placeholdername:blocks/mekanism_infusion_texture'))
+mods.mekanism.infusion.addType('groovy_example', resource('groovyscriptdev:blocks/mekanism_infusion_texture'))
 mods.mekanism.infusion.add(infusionType('diamond'), 100, item('minecraft:clay'))
 mods.mekanism.infusion.add(infusionType('carbon'), 100, item('minecraft:gold_ingot'))
 mods.mekanism.infusion.add('groovy_example', 10, item('minecraft:ice'))
--- examples/preInit/generated/mekanism.groovy
+++ examples/preInit/generated/mekanism.groovy
@@ -1,9 +1,10 @@
 
+// Auto generated groovyscript example file
 // MODS_LOADED: mekanism
+
 import net.minecraftforge.client.event.TextureStitchEvent
 
-if (!isLoaded('mekanism')) return
-println 'mod \'mekanism\' detected, running script'
+log 'mod \'mekanism\' detected, running script'
 
 eventManager.listen { TextureStitchEvent.Pre event ->
     event.getMap().registerSprite(resource('groovyscriptdev:blocks/mekanism_infusion_texture'))
