34a275e53355a1dbb7419abd269497dd7258980d
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-11-21 17:24:34 -0800

    remove copykey fix because its a separate pr

--- src/main/java/com/cleanroommc/groovyscript/keybinds/CopyKey.java
+++ src/main/java/com/cleanroommc/groovyscript/keybinds/CopyKey.java
@@ -82,7 +82,7 @@ public class CopyKey extends GroovyScriptKeybinds.Key {
 
     @Override
     public boolean isValid() {
-        return mc.isIntegratedServerRunning() && mc.player != null;
+        return mc.isIntegratedServerRunning();
     }
 
     // only runs if isIntegratedServerRunning() is true, so getIntegratedServer() cannot be null
