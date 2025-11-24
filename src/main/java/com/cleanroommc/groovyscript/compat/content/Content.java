66c68ca4e9cbc2147dac84e742199e42f4f6ca64
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-11-23 23:09:06 -0800

    document content creation

--- src/main/java/com/cleanroommc/groovyscript/compat/content/Content.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/content/Content.java
@@ -2,7 +2,12 @@ package com.cleanroommc.groovyscript.compat.content;
 
 import com.cleanroommc.groovyscript.GroovyScript;
 import com.cleanroommc.groovyscript.api.GroovyLog;
+import com.cleanroommc.groovyscript.api.documentation.annotations.Example;
+import com.cleanroommc.groovyscript.api.documentation.annotations.MethodDescription;
+import com.cleanroommc.groovyscript.api.documentation.annotations.RecipeBuilderDescription;
+import com.cleanroommc.groovyscript.api.documentation.annotations.RegistryDescription;
 import com.cleanroommc.groovyscript.registry.NamedRegistry;
+import com.cleanroommc.groovyscript.sandbox.LoadStage;
 import groovy.lang.Closure;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -17,10 +22,24 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
+@RegistryDescription(location = LoadStage.PRE_INIT, category = RegistryDescription.Category.CUSTOM)
 public class Content extends NamedRegistry {
 
     public CreativeTabs defaultTab;
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION, example = @Example(value = """
+            'snack', (new ItemFood(20, 10, false) {
+                protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+                    if (!worldIn.isRemote) {
+                        player.addPotionEffect(new PotionEffect(potion('minecraft:regeneration'), 240000, 3, false, false))
+                        player.addPotionEffect(new PotionEffect(potion('minecraft:resistance'), 240000, 3, false, false))
+                    }
+                }
+            }).setAlwaysEdible()""", imports = {
+            "net.minecraft.item.ItemFood",
+            "net.minecraft.potion.PotionEffect",
+            "net.minecraft.entity.player.EntityPlayer"
+    }))
     public void registerItem(@Nullable String name, Item item) {
         if (name != null) {
             item.setRegistryName(GroovyScript.getRunConfig().getPackId(), name);
@@ -31,16 +50,46 @@ public class Content extends NamedRegistry {
         GroovyItem.registerItem(item);
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION)
     public void registerBlock(String name, Block block, ItemBlock item) {
         block.setRegistryName(GroovyScript.getRunConfig().getPackId(), name);
         item.setRegistryName(GroovyScript.getRunConfig().getPackId(), name);
         GroovyBlock.register(block, item);
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION, example = @Example(value = """
+            'dragon_egg_lamp', (new Block(blockMaterial('redstone_light')) {
+                protected static final AxisAlignedBB DRAGON_EGG_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 1.0D, 0.9375D)
+            
+                AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+                    return DRAGON_EGG_AABB
+                }
+            
+                boolean isOpaqueCube(IBlockState state) {
+                    return false
+                }
+            
+                boolean isFullCube(IBlockState state) {
+                    return false
+                }
+            
+                boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+                    return true
+                }
+            
+                BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+                    return BlockFaceShape.UNDEFINED
+                }
+            }).setLightLevel(1.0F)""", imports = {
+            "net.minecraft.block.state.BlockFaceShape",
+            "net.minecraft.util.math.AxisAlignedBB",
+            "net.minecraft.world.IBlockAccess"
+    }))
     public void registerBlock(String name, Block block) {
         registerBlock(name, block, new ItemBlock(block));
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION)
     public void registerFluid(Fluid fluid) {
         FluidRegistry.registerFluid(fluid);
     }
@@ -61,6 +110,7 @@ public class Content extends NamedRegistry {
         return new GroovyFluid.Builder(name);
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION)
     public CreativeTabs createCreativeTab(String name, ItemStack icon) {
         return new CreativeTabs(name) {
 
@@ -71,6 +121,7 @@ public class Content extends NamedRegistry {
         };
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION)
     public CreativeTabs createCreativeTab(String name, Supplier<ItemStack> icon) {
         return new CreativeTabs(name) {
 
@@ -81,6 +132,7 @@ public class Content extends NamedRegistry {
         };
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION, example = @Example("'groovyscript.other_tab_clay', _ -> item('minecraft:clay')"))
     public CreativeTabs createCreativeTab(String name, Closure<ItemStack> icon) {
         return new CreativeTabs(name) {
 
@@ -91,14 +143,18 @@ public class Content extends NamedRegistry {
         };
     }
 
+    @MethodDescription(type = MethodDescription.Type.ADDITION)
     public CreativeTabs createCreativeTab(String name, Item icon) {
         return createCreativeTab(name, new ItemStack(icon));
     }
 
+    @MethodDescription(type = MethodDescription.Type.QUERY)
     public CreativeTabs getDefaultTab() {
         return defaultTab;
     }
 
+    // Set as VALUE so it is the first in the file
+    @MethodDescription(type = MethodDescription.Type.VALUE, example = @Example("content.createCreativeTab('groovyscript.example_creative_tab', _ -> item('groovyscriptdev:heartofauniverse'))"))
     public void setDefaultCreativeTab(CreativeTabs tab) {
         this.defaultTab = tab;
     }
