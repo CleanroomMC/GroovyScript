eecb65d2bc54c40d3e40bc905e033e21e8ec87a6
brachy84 <brachy84@gmail.com>
2026-04-03 14:54:40 +0200

    fix AspectItemStackExpansion

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/aspect/AspectItemStackExpansion.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/thaumcraft/aspect/AspectItemStackExpansion.java
@@ -1,16 +1,17 @@
 package com.cleanroommc.groovyscript.compat.mods.thaumcraft.aspect;
 
 import com.cleanroommc.groovyscript.compat.mods.ModSupport;
+import com.cleanroommc.groovyscript.helper.ingredient.IngredientHelper;
 import net.minecraft.item.ItemStack;
 
 public class AspectItemStackExpansion {
 
     public static void addAspect(ItemStack itemStack, AspectStack aspect) {
-        ModSupport.THAUMCRAFT.get().aspectHelper.add(itemStack, aspect);
+        ModSupport.THAUMCRAFT.get().aspectHelper.add(IngredientHelper.toIIngredient(itemStack), aspect);
     }
 
     public static void removeAspect(ItemStack itemStack, AspectStack aspect) {
-        ModSupport.THAUMCRAFT.get().aspectHelper.remove(itemStack, aspect);
+        ModSupport.THAUMCRAFT.get().aspectHelper.remove(IngredientHelper.toIIngredient(itemStack), aspect);
     }
 
     public static void clearAspects(ItemStack itemStack) {
