87182aceb721d076d26f4afacdcec381227176b1
Lonelyxiya <3438505487@qq.com>
2026-05-07 19:38:08 +0800

    Update Railcraft.java

--- src/main/java/com/cleanroommc/groovyscript/compat/mods/railcraft/Railcraft.java
+++ src/main/java/com/cleanroommc/groovyscript/compat/mods/railcraft/Railcraft.java
@@ -5,8 +5,7 @@ import com.cleanroommc.groovyscript.compat.mods.GroovyPropertyContainer;
 public class Railcraft extends GroovyPropertyContainer {
 
     public final BlastFurnace blastFurnace = new BlastFurnace();
-    public final CokeOven cokeOven = new CokeOven();
-    public final RockCrusher rockCrusher = new RockCrusher();
-    public final RollingMachine rollingMachine = new RollingMachine();
-    public final FluidFuels fluidFuels = new FluidFuels();
-}
+public final CokeOven cokeOven = new CokeOven();
+public final FluidFuels fluidFuels = new FluidFuels();
+public final RockCrusher rockCrusher = new RockCrusher();
+public final RollingMachine rollingMachine = new RollingMachine();
